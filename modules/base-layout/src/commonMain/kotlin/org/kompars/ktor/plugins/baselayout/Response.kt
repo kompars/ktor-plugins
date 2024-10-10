244c57c487090f651ae8e1160156b8cbfe40f1bd
morki <morki@morki.cz>
2024-10-10 12:25:48 +0200

    Add base layout plugin

---
+++ modules/base-layout/src/commonMain/kotlin/org/kompars/ktor/plugins/baselayout/Response.kt
@@ -0,0 +1,26 @@
+package org.kompars.ktor.plugins.baselayout
+
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.utils.io.charsets.*
+import kotlinx.html.*
+import kotlinx.html.stream.*
+
+public suspend fun ApplicationCall.respondBaseLayout(
+    layout: String? = null,
+    status: HttpStatusCode = HttpStatusCode.OK,
+    block: FlowContent.() -> Unit,
+) {
+    val layoutBuilder = attributes[BaseLayoutRegistry].getValue(layout)
+
+    val html = buildString {
+        appendHTML().html { layoutBuilder(this@respondBaseLayout, block) }
+    }
+
+    respondText(
+        text = html,
+        status = status,
+        contentType = ContentType.Text.Html.withCharset(Charsets.UTF_8),
+    )
+}
